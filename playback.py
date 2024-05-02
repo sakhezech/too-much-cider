a5ef595e6edb8217aff1e4c49664ea84c98fc9fc
sakhezech <contact@sakhezech.ru>
2024-05-02 23:17:27 +0300

    slightly more complex playback

--- playback.py
+++ playback.py
@@ -1,11 +1,16 @@
+import os
 import time
 from pathlib import Path
 
-files = Path('./output/').glob('*.txt')
-files = list(files)
-files.sort()
+if __name__ == '__main__':
+    files = Path('./output/').glob('*.txt')
+    files = list(files)
+    files.sort()
 
-frames = [f.read_text() for f in files]
-for frame in frames:
-    print(frame)
-    time.sleep(0.033333)
+    frames = [f.read_text() for f in files]
+    for frame in frames:
+        st = time.perf_counter()
+        os.system('clear||cls')
+        print(frame)
+        et = time.perf_counter()
+        time.sleep(0.033333 - (et - st))
