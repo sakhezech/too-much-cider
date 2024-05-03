5cccce43396dc76632d5b3bc1d853397ee0b5cce
sakhezech <contact@sakhezech.ru>
2024-05-03 03:11:31 +0300

    ability to pass a link to the script

--- prepare_frames.sh
+++ prepare_frames.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env sh
 if [ ! -f "./input.webm" ]; then
-    yt-dlp -o input https://www.youtube.com/watch?v=FtutLA63Cp8
+    yt-dlp -o input ${1:-'https://www.youtube.com/watch?v=FtutLA63Cp8'}
 fi
 mkdir -p ./frames/
 ffmpeg  -i input.webm  \
-    -f lavfi -i color=gray:size=2x2,scale=${1:-17x9} \
-    -f lavfi -i color=black:size=2x2,scale=${1:-17x9} \
-    -f lavfi -i color=white:size=2x2,scale=${1:-17x9} \
-    -filter_complex "[0:v]scale=${1:-17x9},threshold" \
+    -f lavfi -i color=gray:size=2x2,scale=${2:-17x9} \
+    -f lavfi -i color=black:size=2x2,scale=${2:-17x9} \
+    -f lavfi -i color=white:size=2x2,scale=${2:-17x9} \
+    -filter_complex "[0:v]scale=${2:-17x9},threshold" \
     ./frames/frame%04d.png
