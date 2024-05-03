742bc5f14268e07bf8a295348232204239939906
sakhezech <contact@sakhezech.ru>
2024-05-03 04:46:20 +0300

    playback argument parsing

--- playback.py
+++ playback.py
@@ -1,12 +1,29 @@
+import argparse
 import os
 import time
 from pathlib import Path
 
 if __name__ == '__main__':
-    files = Path('./output/').glob('*.txt')
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--dir',
+        type=Path,
+        default=Path('./output/'),
+        help='frame directory (defaults to ./output/)',
+    )
+    parser.add_argument(
+        '--fps',
+        type=int,
+        default=30,
+        help='frames per second (defaults to 30)',
+    )
+    args = parser.parse_args()
+
+    files = args.dir.glob('*.txt')
     files = list(files)
     files.sort()
 
+    freq = 1 / args.fps
     frames = [f.read_text() for f in files]
     for frame in frames:
         st = time.perf_counter()
@@ -14,6 +31,6 @@ if __name__ == '__main__':
         print(frame)
         et = time.perf_counter()
         try:
-            time.sleep(0.033333 - (et - st))
+            time.sleep(freq - (et - st))
         except ValueError:
             pass
