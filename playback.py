ea1c60050f44b696251870e2b2744112da73be7d
sakhezech <contact@sakhezech.ru>
2024-05-07 11:38:37 +0300

    make playback curses for non-windows systems

--- playback.py
+++ playback.py
@@ -1,7 +1,34 @@
 import argparse
 import os
+import sys
 import time
 from pathlib import Path
+
+if sys.platform != 'win32':
+    import curses
+
+
+def playback(stdscr: 'curses._CursesWindow', frames: list[str], nap: float):
+    nap = int(1000 * nap)
+    curses.use_default_colors()
+    for frame in frames:
+        stdscr.clear()
+        stdscr.addstr(frame)
+        stdscr.refresh()
+        curses.napms(nap)
+
+
+def playback_windows(frames: list[str], nap: float):
+    for frame in frames:
+        st = time.perf_counter()
+        os.system('cls')
+        print(frame)
+        et = time.perf_counter()
+        try:
+            time.sleep(nap - (et - st))
+        except ValueError:
+            pass
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -25,12 +52,7 @@ if __name__ == '__main__':
 
     freq = 1 / args.fps
     frames = [f.read_text() for f in files]
-    for frame in frames:
-        st = time.perf_counter()
-        os.system('clear||cls')
-        print(frame)
-        et = time.perf_counter()
-        try:
-            time.sleep(freq - (et - st))
-        except ValueError:
-            pass
+    if sys.platform == 'win32':
+        playback_windows(frames, freq)
+    else:
+        curses.wrapper(playback, frames, freq)
